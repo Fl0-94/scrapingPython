a4c13da7716b61ea9438ea1cd92b09646100d2cb
Fl0-94 <florian.decodtspro@gmail.com>
2024-10-22 16:42:53 +0200

    Phase2

---
+++ Phase2.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+def get_book_urls(category_url):
+    book_urls = []
+    while category_url:
+        response = requests.get(category_url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        for h3 in soup.find_all('h3'):
+            book_urls.append('http://books.toscrape.com/catalogue' + h3.find('a')['href'][8:])
+        next_button = soup.find('li', class_='next')
+        category_url = 'http://books.toscrape.com/catalogue/' + next_button.find('a')['href'] if next_button else None
+    return book_urls
+
+def get_book_data(book_url):
+    response = requests.get(book_url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    product_page_url = book_url
+    upc = soup.find('th', string='UPC').find_next_sibling('td').text
+    title = soup.find('h1').text
+    price_including_tax = soup.find('th', string='Price (incl. tax)').find_next_sibling('td').text
+    price_excluding_tax = soup.find('th', string='Price (excl. tax)').find_next_sibling('td').text
+    number_available = soup.find('th', string='Availability').find_next_sibling('td').text
+    product_description = soup.find('meta', {'name': 'description'})['content'].strip()
+    category = soup.find('ul', class_='breadcrumb').find_all('li')[2].text.strip()
+    review_rating = soup.find('p', class_='star-rating')['class'][1]
+    image_url = soup.find('img')['src']
+    image_url = image_url.replace('../../', 'http://books.toscrape.com/')
+    return [product_page_url, upc, title, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url]
+
+category_url = 'https://books.toscrape.com/catalogue/category/books/horror_31/index.html'
+book_urls = get_book_urls(category_url)
+
+with open('category_books_data.csv', mode='w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['product_page_url', 'universal_product_code (upc)', 'title', 'price_including_tax', 'price_excluding_tax', 'number_available', 'product_description', 'category', 'review_rating', 'image_url'])
+    for book_url in book_urls:
+        writer.writerow(get_book_data(book_url))
---
+++ README.md
@@ -0,0 +1,16 @@
+# OldBooks Finder
+
+Ce projet est un script Python qui permet de récupérer les données de tous les livres d'une catégorie spécifique sur le site **OldBooks Finder** et de les enregistrer dans un fichier CSV.
+
+## Prérequis
+
+Avant de commencer, assurez-vous d'avoir les éléments suivants installés sur votre machine :
+
+- Python 3.x
+- pip (gestionnaire de paquets Python)
+
+## Installation
+
+1. Installer les dépendances 
+
+pip install -r requirements.txt
